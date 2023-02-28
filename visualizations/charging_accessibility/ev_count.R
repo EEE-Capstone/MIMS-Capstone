dd0b31b34d2d84f9c0a0313eba494e9def3f98cb
Clara Hu <77514135+claralhu@users.noreply.github.com>
2023-02-27 23:29:07 -0800

    Update ev_count.R

--- visualizations/charging_accessibility/ev_count.R
+++ visualizations/charging_accessibility/ev_count.R
@@ -1,4 +1,7 @@
 ca_ev_registrations_public <- read_csv("ca_ev_registrations_public.csv") ## Data from: https://www.atlasevhub.com/materials/state-ev-registration-data/#data
+county_geoid_map <- read_csv("county_geoid_map.csv")
+
+
 counties <- unique(ca_ev_registrations_public$`County GEOID`)
 count_ev <- numeric(0)
 for (i in 1:length(counties)) {
@@ -8,5 +11,10 @@ for (i in 1:length(counties)) {
 
 EV_counts <- data.frame(county_geoid = counties, 
                         counts = count_ev)
+
+EV_counts <- EV_counts[order(EV_counts$county_geoid), ]
+EV_counts$county <- county_geoid_map[order(county_geoid_map$GEOID),]$County
+EV_counts <- EV_counts[c("county_geoid","county","counts") ]
+
 EV_counts
 write.csv(EV_counts, "EVcounts.csv")
