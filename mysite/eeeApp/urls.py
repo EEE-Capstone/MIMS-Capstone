b3ebc625bb491a9afa915b2c5054100cdcab4fd0
Clara Hu <clarahu99@gmail.com>
2023-05-11 11:08:34 -0700

    orgnization

--- mysite/eeeApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('test/', views.test, name='test'),
-    path('testForm/', views.get_name, name='getName')
-]
